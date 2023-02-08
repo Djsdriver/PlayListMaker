94ed92d35254f1a6d6310d0f7b8adb05fe922843
Djsdriver <alexei.gra4ev@gmail.com>
2023-02-08 10:47:56 +0300

    Домашняя работа Sprint_8

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.playlistmaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnMedia.setOnClickListener {
+            startActivity(Intent(this,MediaScreen::class.java))
+        }
+
+        binding.btnSearch.setOnClickListener {
+            startActivity(Intent(this, SearchScreen::class.java))
+
+        }
+
+        binding.btnSetting.setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
+
+
     }
+
+
+
 }
